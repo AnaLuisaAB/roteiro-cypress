5fdf3d8b5fb9b63e97c251fb470f88732e3ae284
AnaLuisaAB <ana.araujo@dcc.ufmg.br>
2024-06-26 10:13:29 -0300

    Adicionando 3 novos testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,37 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+  it('Deleta todas as tarefas completas', () => {
+    cy.visit('http://127.0.0.1:7001');
+    cy.get('.new-todo')
+      .type('TP2 de ES{enter}')
+      .type('Prova de ES{enter}');
+    cy.get('.todo-list li .toggle')
+    .first()
+    .click();
+    cy.get('.todo-list li .toggle')
+    .last()
+    .click();
+    cy.contains('Clear completed').click();
+    cy.get('.todo-list li')
+      .should('have.length', 0)
+  });
+  
+  it('Edita uma tarefa', () => {
+    cy.visit('http://127.0.0.1:7001');
+    cy.get('.new-todo')
+      .type('TP2{enter}');
+    cy.get('.todo-list li label')
+      .dblclick();
+      cy.get('.edit')
+      .type(' de Engenharia de Software{enter}');
+    cy.get('.todo-list li')
+      .should('have.text', 'TP2 de Engenharia de Software');
+  });
+  
+  it('Testa se inicializa com 0 tarefas', () => {
+    cy.visit('http://127.0.0.1:7001');
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+  });
 });
